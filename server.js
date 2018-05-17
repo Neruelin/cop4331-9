463fcf2ee775cfff4e152f940853c4d009ddbc2a
Tyler <neruelin@gmail.com>
2018-05-17 18:48:23 -0400

    Reorganization and comments
    Reorganized static directories, added comments

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
+const path = require('path');
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
+
+const app = express(); // main app object
+
+const port = process.env.PORT || 8080; // uses server env port if exists, else uses default 8080
+
+/* defining static content directories
+   Eg: accessing "domain.com/views" will actually access "server_directory/public/html/"
+   frontend dir name            backend dir name
+		   |			                |          */
+app.use("/views", express.static('public/html/'));
+app.use("/style", express.static('public/css'));
+app.use("/js", express.static('public/js'));
+app.use("/", express.static('public'));
+
+// routes
+
+// for homepage get requests
+app.get('/', function (req, res) {
+	console.log("Serving index.html");
+	res.sendFile(__dirname + '/public/html/index.html');
+});
 
 
-app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('index.html'));
 
+// start app on port
 app.listen(port, () => console.log("active on port: " + port));
