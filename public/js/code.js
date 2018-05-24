f9731843551d378a224cb219c3aea06d647009ad
TYLER DUNCAN <chico55_8008@yahoo.com>
2018-05-23 23:53:00 -0400

    Add files via upload

--- public/js/code.js
+++ public/js/code.js
@@ -6,28 +6,13 @@ const urlBase = 'https://cop4331-9.herokuapp.com'
 //   Log in.
 function login()
 {
-     var login = document.getElementById("user-login").value;
-	var password = document.getElementById("password-login").value;
-     var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
-	var url = urlBase + '/login';
+     // var login = document.getElementById("user-login").value;
+	// var password = document.getElementById("password-login").value;
+     // var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+	// var url = urlBase + '/login';
 
-     let data = {
-          name: login,
-          pw: pasword
-     }
+     fetch('https://cop4331-9.herokuapp.com/login');
 
-     let fetchData = {
-          method: 'POST',
-          body: data,
-          headers: new Headers()
-     }
-
-     fetch(url, fetchData)
-     .then(function() {
-          // Handle response you get from the server
-     });
-
-     var xhr = new XMLHttpRequest();
      //   Hide all login fields
      hideOrShow('log-in', false);
      hideOrShow('Signup-btn', false);
@@ -63,20 +48,11 @@ function signup()
 
 function createAccount()
 {
-     //   Get both password field values, and check if they match.
-     var password = document.getElementById("password-signup").value;
-     var rePass = document.getElementById("repassword-signup").value;
-
-     //   Check to see if this can be done in real time as the password
-     //   is being typed.
-     if(password == rePass)
+     fetch('https://cop4331-9.herokuapp.com/signup')
+     .then(function(response))
      {
-          var firstName = document.getElementById("firstName").value;
-          var lastName = document.getElementById("lastName").value;
-          var email = document.getElementById("email").value;
-          var userName = document.getElementById("user-signup").value;
+          console.log(response);
      }
-
      //   Hide all Sign up fields
      hideOrShow('sign-up', false);
 
