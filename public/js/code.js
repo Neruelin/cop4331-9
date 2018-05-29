3832799f62f4b266d196de643d4225cd22d24fe5
Tyler <neruelin@gmail.com>
2018-05-28 23:23:54 -0400

    mega progress

--- public/js/code.js
+++ public/js/code.js
@@ -26,6 +26,7 @@ function login()
      console.log(JSON.stringify(userdata));
 
      $.post(url_login, userdata, function (res, status) {
+          console.log(status);
           if (status != "success") {
                displayErr();
                return;
