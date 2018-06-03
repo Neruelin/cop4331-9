17230807b967de88ee10139d659c5325ac15be39
uulltt <ajvark@gmail.com>
2018-06-02 23:33:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -116,8 +116,7 @@ app.post('/login', function (req, response) {
 					if (same) {
 						req.session.loggedin = true;
 						console.log("redirecting to dash");
-						response.redirect('/dashboard');
-						return;
+						response.redirect('/dashboard').end();
 					} else {
 						console.log("status 401 ln 122");	
 						response.status(401).end();
