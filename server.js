7579633aa3ac74fde6e52a8169123d0290286813
uulltt <ajvark@gmail.com>
2018-06-02 23:49:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/login', function (req, response) {
 					if (same) {
 						req.session.loggedin = true;
 						console.log("redirecting to dash");
-						response.sendFile(__dirname + '/public/html/dashboard.html');
+						//response.sendFile(__dirname + '/public/html/dashboard.html');
 						response.redirect('/dashboard');
 						return;
 						//return;
