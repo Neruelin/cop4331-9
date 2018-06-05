40cdbb7c980234e22a03af340229355b902c8eec
Tyler <neruelin@gmail.com>
2018-06-05 07:40:02 -0400

    a

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -40,6 +40,7 @@ function displayContact()
 {
 	var res = results[index];
 
+	$('#indexed').text((index+1) + "/" + results.length)
 	$('#Name').text(res.fname + ' ' + res.lname);
 	$('#Phone').text(res.phonenumber);
 	$('#Email').text(res.email);
