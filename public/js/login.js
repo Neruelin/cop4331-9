fa1870950cbdf10807165340a94427bef46ca112
uulltt <ajvark@gmail.com>
2018-06-06 18:27:42 -0400

    taking out extra code bits

--- public/js/login.js
+++ public/js/login.js
@@ -17,7 +17,7 @@ function login()
 
      console.log(JSON.stringify(userdata));
      $.post(url_login, userdata, function (res, status) {
-		id = res;
+		//id = res;
 	  window.location = '/dashboard';
      }).fail(function () {
           displayErr();
